e750dfb504779079468acb32dcdc6d950297e170
Senih <40578755+senihucar@users.noreply.github.com>
2020-11-08 21:11:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import assignment2
+import functions
 
 if __name__ == '__main__':
-    assignment2.open_and_write_txt_file('\\\\\search by city and bedroom number (Vancouver,3)\\\\\\\ \n')
-    assignment2.search_by_city_and_bedroom_number('Vancouver', 3)
-    assignment2.open_and_write_txt_file('\n\\\\\search by price range(550000,1000000 )\\\\\\\ \n')
-    assignment2.search_by_price_range(550000, 1000000)
-    assignment2.open_and_write_txt_file('\n\\\\\search by city and property type(Burnaby,House)\\\\\\\ \n')
-    assignment2.search_by_city_and_property_type('Burnaby', 'House')
-    assignment2.open_and_write_txt_file('\n\\\\\ reduce price\\\\\\\ \n')
-    assignment2.reduce_price()
-    assignment2.open_and_write_txt_file('\n\\\\\search_by_postal_code_range(V,7)\\\\\\\ \n')
-    assignment2.search_by_postal_code_range('V', '7')
-    assignment2.open_and_write_txt_file('\n\\\\\Details of the MLS : R2508895 : are\\\\\\\ \n')
-    assignment2.get_listing('R2508895')
+    functions.open_and_write_txt_file('\\\\\search by city and bedroom number (Vancouver,3)\\\\\\\ \n')
+    functions.search_by_city_and_bedroom_number('Vancouver', 3)
+    functions.open_and_write_txt_file('\n\\\\\search by price range(550000,1000000 )\\\\\\\ \n')
+    functions.search_by_price_range(550000, 1000000)
+    functions.open_and_write_txt_file('\n\\\\\search by city and property type(Burnaby,House)\\\\\\\ \n')
+    functions.search_by_city_and_property_type('Burnaby', 'House')
+    functions.open_and_write_txt_file('\n\\\\\ reduce price\\\\\\\ \n')
+    functions.reduce_price()
+    functions.open_and_write_txt_file('\n\\\\\search_by_postal_code_range(V,7)\\\\\\\ \n')
+    functions.search_by_postal_code_range('V', '7')
+    functions.open_and_write_txt_file('\n\\\\\Details of the MLS : R2508895 : are\\\\\\\ \n')
+    functions.get_listing('R2508895')
