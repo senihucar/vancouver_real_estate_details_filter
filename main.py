1a9e126e184f416042576dc75993fef7c326032b
Senih <40578755+senihucar@users.noreply.github.com>
2020-11-08 21:10:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import assignment2
 
 if __name__ == '__main__':
@@ -18,4 +13,3 @@ if __name__ == '__main__':
     assignment2.search_by_postal_code_range('V', '7')
     assignment2.open_and_write_txt_file('\n\\\\\Details of the MLS : R2508895 : are\\\\\\\ \n')
     assignment2.get_listing('R2508895')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
